b34886c06920a8ac9e2d103c8fbb257017d2ae33
Hadar Rottenberg <hadarro@etoro.com>
2019-04-01 10:27:10 +0300

    add: deploy new version 0.0.12

--- truffle.js
+++ truffle.js
@@ -56,7 +56,7 @@ module.exports = {
         return new HDWalletProvider(mnemonic, "https://rpc.fuse.io/",0,10)
       },
       network_id: 121,
-      gas:5700000,
+      gas:4500000,
       skipDryRun:true,
       gasPrice:1000000000 //1 gwei
     }

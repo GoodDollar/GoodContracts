782b0268a66c38bf2940ddffa5d33eb8e0ea89e4
U-TRAD\hadarro <hadar@gooddollar.org>
2019-04-22 15:15:03 +0300

    add: version release

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # npm run migarate_reset:ganache
 # export NETWORK='kovan'
 # npm run migarate_reset:ganache
-./minimize.sh build/contracts
+minimize.sh build/contracts
 git add build/contracts/*
 git commit -a -m "add: version release"
 npm version patch

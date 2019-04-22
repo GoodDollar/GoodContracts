fa3fa1ff0a2c829d01f5ff57f0d7804c1d685415
U-TRAD\hadarro <hadar@gooddollar.org>
2019-04-22 15:14:28 +0300

    add: version release

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-[[ -z $(git status -s) ]] || echo "Error git not clean" && exit 1
-export NETWORK='fuse'
-npm run migarate_reset:ganache
-export NETWORK='kovan'
-npm run migarate_reset:ganache
-./minimize.sh
+# [[ -z $(git status -s) ]] || echo "Error git not clean" && exit 1
+# export NETWORK='fuse'
+# npm run migarate_reset:ganache
+# export NETWORK='kovan'
+# npm run migarate_reset:ganache
+./minimize.sh build/contracts
 git add build/contracts/*
 git commit -a -m "add: version release"
 npm version patch

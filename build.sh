8fcb7759e4aa6b4ae3ceaecad2fae7551221cd43
Hadar Rottenberg <hadar@gooddollar.org>
2019-05-19 16:11:39 +0300

    add: version release

--- build.sh
+++ build.sh
@@ -1,15 +1,47 @@
-[[ -z $(git status -s) ]] || (echo "Error git not clean" && exit 1)
-[[ -z $MNEMONIC ]] || (echo "Error MNEMONIC missing no env?" && exit 1)
-if [  $1 == 'deploy' ]; then 
+set -o allexport
+source .env
+set +o allexport
+echo $MNEMONIC
+#exit on any command failure
+set -e
+CHANGES=`git status --porcelain --untracked-files=no -s`
+if [[ -z $CHANGES ]]
+then 
+    echo "Error git not clean";
+    exit -1;
+fi
+if [[ -z $MNEMONIC_STAGING || -z $INFURA_API ]]
+then
+    echo "Error MNEMONIC missing no env?";
+    exit -1;
+fi
+if [[  $1 == 'deploy' ]]
+then 
+    export MNEMONIC = $MNEMONIC_STAGING
+    echo "deploying to fuse"
     export NETWORK='fuse'
-    npm run migrate_reset:ganache
+    # npm run migrate_reset:ganache
+    echo "deploying to kovan"
     export NETWORK='kovan'
     npm run migrate_reset:ganache
+    read -p "deploy to production? " prompt
+    if [[ $MNEMONIC_PROD && $prompt =~ [yY](es)* ]]
+    then
+        export MNEMONIC = $MNEMONIC_PROD
+        export NETWORK='fuse'
+        npm run migrate_reset:ganache
+    fi
+
+        
+    
 fi
-./minimize.sh build/contracts
-git add build/contracts/*
-git commit -a -m "add: version release"
-npm version patch
-git push --follow-tags
-npm pack
-npm publish
+read -p "Are you sure you want to continue? <y/N> " prompt
+if [[ $prompt =~ [yY](es)* ]]
+then
+    git add build/contracts/*
+    git commit -a -m "add: version release"
+    npm version patch
+    git push --follow-tags
+    npm pack
+    npm publish
+fi

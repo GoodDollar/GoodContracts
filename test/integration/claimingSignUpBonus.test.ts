8d211e2b0debe860bc2e335e84ada03348c72d20
Mads Jørgensen <mads.da.mind@hotmail.com>
2019-06-18 09:50:01 +0200

    fix: change signer bonus name to sign up bonus

--- test/integration/claimingSignUpBonus.test.ts
+++ test/integration/claimingSignUpBonus.test.ts
@@ -7,7 +7,7 @@ const GoodDollar = artifacts.require("GoodDollar");
 const ControllerInterface = artifacts.require("ControllerInterface");
 const AbsoluteVote = artifacts.require("AbsoluteVote");
 const SchemeRegistrar = artifacts.require("SchemeRegistrar");
-const SignerBonus = artifacts.require("SignerBonus");
+const SignUpBonus = artifacts.require("SignUpBonus");
 
 contract("Integration - Claiming signer bonus", ([founder, claimer, claimer2, nonClaimer]) => {
 
@@ -16,7 +16,7 @@ contract("Integration - Claiming signer bonus", ([founder, claimer, claimer2, no
     let controller: helpers.ThenArg<ReturnType<typeof ControllerInterface['new']>>;
     let absoluteVote: helpers.ThenArg<ReturnType<typeof AbsoluteVote['new']>>;
     let token: helpers.ThenArg<ReturnType<typeof GoodDollar['new']>>;
-    let signerBonus: helpers.ThenArg<ReturnType<typeof SignerBonus['new']>>;
+    let signUpBonus: helpers.ThenArg<ReturnType<typeof SignUpBonus['new']>>;
 
     let proposalId: string;
 
@@ -26,19 +26,19 @@ contract("Integration - Claiming signer bonus", ([founder, claimer, claimer2, no
       controller = await ControllerInterface.at(await avatar.owner());
       absoluteVote = await AbsoluteVote.deployed();
       token = await GoodDollar.at(await avatar.nativeToken());
-      signerBonus = await SignerBonus.new(avatar.address, identity.address, 5);
+      signUpBonus = await SignUpBonus.new(avatar.address, identity.address, 5);
 
       await identity.addClaimer(claimer);
       await identity.addClaimer(claimer2);
     });
 
     it("should not allow claimer to claim before starting scheme", async () => {
-      await helpers.assertVMException(signerBonus.claim({ from: claimer }), "scheme is not registered")
+      await helpers.assertVMException(signUpBonus.claim({ from: claimer }), "scheme is not registered")
     })
 
-    it("should start SignerBonus scheme", async () => {
+    it("should start SignUpBonus scheme", async () => {
       const schemeRegistrar = await SchemeRegistrar.deployed();
-      const transaction = await schemeRegistrar.proposeScheme(avatar.address, signerBonus.address,
+      const transaction = await schemeRegistrar.proposeScheme(avatar.address, signUpBonus.address,
         helpers.NULL_HASH, "0x0000010", helpers.NULL_HASH);
 
       proposalId = transaction.logs[0].args._proposalId;
@@ -51,25 +51,25 @@ contract("Integration - Claiming signer bonus", ([founder, claimer, claimer2, no
     });
 
     it("should not allow non-claimer to claim", async () => {
-      await helpers.assertVMException(signerBonus.claim({ from: nonClaimer }), "is not claimer");
+      await helpers.assertVMException(signUpBonus.claim({ from: nonClaimer }), "is not claimer");
     })
 
     it("should allow claimer to claim", async () => {
       let oldBalance = await token.balanceOf(claimer);
       expect(oldBalance.toString()).to.be.equal("0");
-      await signerBonus.claim({ from: claimer });
+      await signUpBonus.claim({ from: claimer });
 
       let newBalance = await token.balanceOf(claimer);
       expect(newBalance.toString()).to.be.equal("5");
     })
 
     it("should not allow claimer to claim twice", async() => {
-      await helpers.assertVMException(signerBonus.claim({ from: claimer}), "has already claimed");
+      await helpers.assertVMException(signUpBonus.claim({ from: claimer}), "has already claimed");
     });
 
-    it("should end SignerBonus scheme", async () => {
+    it("should end SignUpBonus scheme", async () => {
       const schemeRegistrar = await SchemeRegistrar.deployed();
-      const transaction = await schemeRegistrar.proposeToRemoveScheme(avatar.address, signerBonus.address,
+      const transaction = await schemeRegistrar.proposeToRemoveScheme(avatar.address, signUpBonus.address,
           helpers.NULL_HASH);
 
       proposalId = transaction.logs[0].args._proposalId;
@@ -81,7 +81,7 @@ contract("Integration - Claiming signer bonus", ([founder, claimer, claimer2, no
     });
 
     it("should not allow claimer to claim after scheme is unregistered", async () => {
-      await helpers.assertVMException(signerBonus.claim({ from: claimer2 }), "scheme is not registered");
+      await helpers.assertVMException(signUpBonus.claim({ from: claimer2 }), "scheme is not registered");
     })
 });
 

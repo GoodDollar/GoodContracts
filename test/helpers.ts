989dc830f7870945a9669b03a7dca8cc17e823e6
Mads Jørgensen <mads.da.mind@hotmail.com>
2019-06-19 16:02:43 +0200

    fix: prepare for deployment

--- test/helpers.ts
+++ test/helpers.ts
@@ -7,10 +7,10 @@ export const SOME_ADDRESS = '0x1000000000000000000000000000000000000000';
 export const increaseTime = async function(duration) {
   const id = await Date.now();
 
-   web3.providers.HttpProvider.prototype.sendAsync = web3.providers.HttpProvider.prototype.send;
+  web3.providers.HttpProvider.prototype.sendAsync = web3.providers.HttpProvider.prototype.send;
 
   return new Promise((resolve, reject) => {
-    web3.currentProvider.sendAsync({
+    (web3 as any).currentProvider.sendAsync({
       jsonrpc: '2.0',
       method: 'evm_increaseTime',
       params: [duration],
@@ -18,7 +18,7 @@ export const increaseTime = async function(duration) {
     }, err1 => {
       if (err1) return reject(err1);
 
-      web3.currentProvider.sendAsync({
+      (web3 as any).currentProvider.sendAsync({
         jsonrpc: '2.0',
         method: 'evm_mine',
         id: id + 1,

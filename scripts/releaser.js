6b1c035eedb09147aa5f572cf092c7ad9e035461
Mads Jørgensen <mads.da.mind@hotmail.com>
2019-08-26 11:08:02 +0200

    add: write deployed contract addresses to JSON file

---
+++ scripts/releaser.js
@@ -0,0 +1,20 @@
+const fse = require("fs-extra");
+
+module.exports = function(deployment, network) {
+	const dir = "releases/";
+	console.log("releaser:", { network, dir });
+	return fse.readFile(dir + "/deployment.json", "utf8", function(err, data) {
+		let previousDeployment = {};
+		if (!err) {
+			previousDeployment = JSON.parse(data);
+		}
+		return fse.ensureDir(dir).then(() => {
+			let finalDeployment = { ...previousDeployment, [network]: deployment };
+			return fse.writeFile(
+				dir + "/deployment.json",
+				JSON.stringify(finalDeployment),
+				"utf8"
+			);
+		});
+	});
+};

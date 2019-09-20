e9f274af6f97aa8e0a7d27ce5080195d703b8353
Hadar <hadarr@gmail.com>
2019-09-20 11:45:54 +0300

    fix: ubi deployment params

--- migrations/3_deploy_new_ubi.js
+++ migrations/3_deploy_new_ubi.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 const { toGD } = require("./helpers");
+=======
+const toGD = stringAmount => (parseInt(stringAmount) * 100).toString();
+>>>>>>> fix: ubi deployment params
 const settings = require("./deploy-settings.json");
 const Identity = artifacts.require("./Identity");
 const Controller = artifacts.require("./Controller.sol");

6225d73f92f7a92ca41d124f82f72193c4cfc820
Hadar <hadarr@gmail.com>
2019-09-21 23:26:37 +0300

    fix: missing comma, redudant await

--- contracts/dao/schemes/FixedUBI.sol
+++ contracts/dao/schemes/FixedUBI.sol
@@ -63,7 +63,7 @@ contract FixedUBI is AbstractUBI {
             now.sub(lastClaimed[msg.sender]) / 1 days; 
         
         claimDays = claimDays > 7 ? 7 : claimDays;
-        uint256 amount = claimDistribution
+        uint256 amount = claimDistribution;
         return amount.mul(claimDays);
     }
 

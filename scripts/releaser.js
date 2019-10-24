849d956fb46dad5d7868ee2040e1266f674c8820
Hadar Rottenberg <hadar@gooddollar.org>
2019-10-24 23:03:12 +0300

    refactor: package.json deps and releaser logging

--- scripts/releaser.js
+++ scripts/releaser.js
@@ -1,20 +1,21 @@
 const fse = require("fs-extra");
 
 module.exports = function(deployment, network) {
-	const dir = "releases/";
-	console.log("releaser:", { network, dir });
-	return fse.readFile(dir + "/deployment.json", "utf8", function(err, data) {
-		let previousDeployment = {};
-		if (!err) {
-			previousDeployment = JSON.parse(data);
-		}
-		return fse.ensureDir(dir).then(() => {
-			let finalDeployment = { ...previousDeployment, [network]: deployment };
-			return fse.writeFile(
-				dir + "/deployment.json",
-				JSON.stringify(finalDeployment),
-				"utf8"
-			);
-		});
-	});
+  const dir = "releases/";
+  console.log("releaser:", { network, dir });
+  return fse.readFile(dir + "/deployment.json", "utf8", function(err, data) {
+    let previousDeployment = {};
+    if (!err) {
+      previousDeployment = JSON.parse(data);
+    }
+    return fse.ensureDir(dir).then(() => {
+      let finalDeployment = { ...previousDeployment, [network]: deployment };
+      console.log("releaser:", { previousDeployment, finalDeployment });
+      return fse.writeFile(
+        dir + "/deployment.json",
+        JSON.stringify(finalDeployment),
+        "utf8"
+      );
+    });
+  });
 };

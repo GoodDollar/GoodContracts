99d51ba7178fe7acf6fc56fb90fc35af40f04c32
Hadar Rottenberg <hadar@gooddollar.org>
2019-10-27 15:52:08 +0200

    add: dynamic fee formula + etoro defaults

--- contracts/dao/schemes/FeeFormula.sol
+++ contracts/dao/schemes/FeeFormula.sol
@@ -18,11 +18,18 @@ contract AbstractFees is SchemeGuard {
 contract FeeFormula is AbstractFees {
     using SafeMath for uint256;
 
+    uint256 public percentage;
+    
+    constructor (uint256 _percentage) public {
+        require(_percentage<100, "Percentage should be <100");
+        percentage = _percentage;
+    }
+
     /* @dev calculates 1 percentage fee of given value. Must be registered as a scheme.
      * @param _value the value to calculate fees from
      * @return the transactional fee for given value
      */
     function getTxFees(uint256 _value) public view returns (uint256) {
-        return _value.div(100);
+        return _value.div(100).mul(percentage);
     }
 }

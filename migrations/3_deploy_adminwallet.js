449704d4781e60d5e6118763cf71ce8762a9b766
Hadar Rottenberg <hadar@gooddollar.org>
2019-11-07 00:02:33 +0200

    add: version prerelease

--- migrations/3_deploy_adminwallet.js
+++ migrations/3_deploy_adminwallet.js
@@ -1,3 +1,4 @@
+const Web3 = require("web3");
 const AdminWallet = artifacts.require("./AdminWallet.sol");
 const settings = require("./deploy-settings.json");
 // if (process.env.NODE_ENV !== 'production') { // https://codeburst.io/process-env-what-it-is-and-why-when-how-to-use-it-effectively-505d0b2831e7
@@ -10,41 +11,52 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 const admin_mnemonic = process.env.ADMIN_MNEMONIC;
 const infura_api = process.env.INFURA_API;
 
-module.exports = async function(deployer, network) {
+module.exports = async function(deployer, network, accounts) {
+  if (network != "ganache" && network != "test" && network != "coverage") {
+    const adminWallet = await AdminWallet.deployed();
+    const networkSettings = settings[network] || settings["default"];
 
-	if( network != 'ganache' && network != 'test'
-		&& network != 'coverage') {
+    let oldProvider = web3.currentProvider;
+    let adminProvider;
 
-		const adminWallet = await AdminWallet.deployed();
-		const networkSettings = settings[network] || settings["default"];
-		
-		let oldProvider = await web3.currentProvider();
-		let adminProvider;
+    const adminWalletBalance = await web3.eth
+      .getBalance(adminWallet.address)
+      .then(parseInt);
+    const adminWalletValue = web3.utils.toWei(
+      networkSettings.walletTransfer,
+      networkSettings.walletTransferUnits
+    );
+    console.log({ adminWalletBalance });
+    if (adminWalletBalance <= parseInt(adminWalletValue))
+      await web3.eth.sendTransaction({
+        to: adminWallet.address,
+        from: accounts[0],
+        value: adminWalletValue
+      });
 
-		await web3.eth.sendTransaction({
-		  to: adminWallet.address,
-		  from: accounts[0],
-		  value: web3.utils.toWei(networkSettings.walletTransfer, networkSettings.walletTransferUnits)
-		});
+    if (["mainnet", "ropsten", "kovan"].includes("network")) {
+      adminProvider = new HDWalletProvider(
+        admin_mnemonic,
+        "https://" + network + ".infura.io/v3/" + infura_api,
+        0,
+        50
+      );
+    } else {
+      adminProvider = new HDWalletProvider(
+        admin_mnemonic,
+        "https://rpc.fusenet.io/",
+        0,
+        50
+      );
+    }
 
-		switch(network) {
-			case 'mainnet' || 'ropsten' || 'kovan':
-			  adminProvider = await new HDWalletProvider(admin_mnemonic, "https://" + network + ".infura.io/v3/" + infura_api, 0, 50);
-			  break;
-			case 'fuse' || 'staging' ||
-			  'etoro' || 'production':
-			  adminProvider = await new HDWalletProvider(admin_mnemonic, "https://rpc.fusenet.io/", 0, 50);
-			  break;
-			default:
-		}
+    console.log({ adminProvider });
+    //web3.setProvider(adminProvider);
+    const adminsWeb3 = new Web3(adminProvider);
+    const admins = await adminsWeb3.eth.getAccounts();
+    console.log({ admins });
 
-		web3.setProvider(adminProvider);
-
-		const admins = await web3.eth.getAccounts()
-
-		web3.setProvider(oldProvider);
-
-		await adminWallet.addAdmins(admins);
-		await adminWallet.topAdmins(1);
-	}
-}
+    await adminWallet.addAdmins(admins);
+    await adminWallet.topAdmins(1);
+  }
+};

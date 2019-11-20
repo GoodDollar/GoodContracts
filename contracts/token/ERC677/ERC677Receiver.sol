fac79ae3198fc50799d765cab1ad89eba9d5e9b0
Mads Jørgensen <mads.da.mind@hotmail.com>
2019-11-20 16:39:40 +0100

    fix: change ERC677 and ERC677Receiver to interfaces

--- contracts/token/ERC677/ERC677Receiver.sol
+++ contracts/token/ERC677/ERC677Receiver.sol
@@ -1,5 +1,5 @@
 pragma solidity 0.5.4;
 
-contract ERC677Receiver {
+interface ERC677Receiver {
     function onTokenTransfer(address _from, uint256 _value, bytes calldata _data) external returns(bool);
 }

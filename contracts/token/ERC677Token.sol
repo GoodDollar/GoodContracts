fdf3e6725e8a3f24c8007d70fc51aed4d7032980
Mads Jørgensen <mads.da.mind@hotmail.com>
2019-12-10 13:05:10 +0100

    fix: remove redundant require statement

--- contracts/token/ERC677Token.sol
+++ contracts/token/ERC677Token.sol
@@ -29,7 +29,7 @@ contract ERC677Token is ERC677, DAOToken, ERC20Pausable {
         whenNotPaused
         returns (bool)
     {
-        require(super.transfer(_to, _value), "Transfer failed");
+        super.transfer(_to, _value);
         emit Transfer(msg.sender, _to, _value, _data);
       
         if (isContract(_to)) {

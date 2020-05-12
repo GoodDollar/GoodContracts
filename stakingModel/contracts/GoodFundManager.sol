2f00710bed85ffea52815100acc11157e6b8b2fb
Hadar Rottenberg <hadar@gooddollar.org>
2020-05-12 17:06:44 +0300

    add: interest donated support and transfer to avatar via bridge

--- stakingModel/contracts/GoodFundManager.sol
+++ stakingModel/contracts/GoodFundManager.sol
@@ -7,34 +7,41 @@ import "../../contracts/dao/schemes/FeelessScheme.sol";
 import "../../contracts/dao/schemes/ActivePeriod.sol";
 import "./GoodReserveCDai.sol";
 
+
 interface StakingContract {
-    function collectUBIInterest(address recipient) external returns (uint256, uint256, uint256);
+    function collectUBIInterest(address recipient)
+        external
+        returns (uint256, uint256, uint256, uint32);
 }
 
+
 /**
-* @title GoodFundManager contract that transfer interest from the staking contract
-* to the reserve contract and transfer the return mintable tokens to the staking
-* contract
-* cDAI support only
-*/
+ * @title GoodFundManager contract that transfer interest from the staking contract
+ * to the reserve contract and transfer the return mintable tokens to the staking
+ * contract
+ * cDAI support only
+ */
 contract GoodFundManager is FeelessScheme, ActivePeriod {
     using SafeMath for uint256;
 
     ERC20 cDai;
     GoodReserveCDai public reserve;
+    address public bridgeContract;
+    address public homeAvatar;
 
     // // tracking the daily withdraws and the actual amount
     // // at the begining of the trading day.
     // mapping (uint256 => Funds) public dailyFunds;
 
-
-    event FundsTransferred(address indexed caller,
-                           address indexed staking,
-                           address indexed reserve,
-                           uint256 cdaiGains,
-                           uint256 gdInterest,
-                           uint256 gdUBI);
-
+    event FundsTransferred(
+        address indexed caller,
+        address indexed staking,
+        address indexed reserve,
+        uint256 cDAIinterestEarned,
+        uint256 cDAIinterestDonated,
+        uint256 gdInterest,
+        uint256 gdUBI
+    );
 
     modifier reserveHasInitialized {
         require(address(reserve) != address(0), "reserve has not initialized");
@@ -44,23 +51,20 @@ contract GoodFundManager is FeelessScheme, ActivePeriod {
     constructor(
         address _cDai,
         Avatar _avatar,
-        Identity _identity
-    )
-        public
-        FeelessScheme(_identity, _avatar)
-        ActivePeriod(now, now * 2)
-        {
+        Identity _identity,
+        address _bridgeContract,
+        address _homeAvatar
+    ) public FeelessScheme(_identity, _avatar) ActivePeriod(now, now * 2) {
         cDai = ERC20(_cDai);
+        bridgeContract = _bridgeContracts;
+        homeAvatar = _homeAvatar;
         start();
     }
 
     /* @dev Start function. Adds this contract to identity as a feeless scheme.
      * Can only be called if scheme is registered
      */
-    function start()
-        public
-        onlyRegistered
-    {
+    function start() public onlyRegistered {
         addRights();
         super.start();
     }
@@ -71,6 +75,19 @@ contract GoodFundManager is FeelessScheme, ActivePeriod {
      */
     function setReserve(GoodReserveCDai _reserve) public onlyAvatar {
         reserve = _reserve;
+    }
+
+    /**
+     * @dev sets the token bridge address on mainnet and the recipient of minted UBI (avatar on sidechain)
+     * @param _bridgeContract address
+     * @param _avatar address
+     */
+    function setBridgeAndHomeAvatar(address _bridgeContract, address _avatar)
+        public
+        onlyAvatar
+    {
+        bridgeContract = _bridgeContract;
+        homeAvatar = _avatar;
     }
 
     /**
@@ -89,33 +106,51 @@ contract GoodFundManager is FeelessScheme, ActivePeriod {
         // cdai balance of the reserve contract
         uint256 currentBalance = cDai.balanceOf(address(reserve));
         // collects the interest from the staking contract and transfer it directly to the reserve contract
-        staking.collectUBIInterest(address(reserve));
+        //collectUBIInterest returns (cdaigains, daigains, precission loss, donation ratio)
+        (, , , uint32 donationRatio) = staking.collectUBIInterest(
+            address(reserve)
+        );
+
         // finds the actual transferred cdai
-        uint256 actualCDaiGains = cDai.balanceOf(address(reserve)).sub(currentBalance);
-        if(actualCDaiGains > 0) {
+        uint256 actualCDaiGains = cDai.balanceOf(address(reserve)).sub(
+            currentBalance
+        );
+        if (actualCDaiGains > 0) {
+            uint256 interestDonated = interest.mul(donationRatio).div(1e6);
+            uint256 afterDonation = interest.sub(interestDonated);
             // mints gd while the interest amount is equal to the transferred amount
-            (uint256 gdInterest, uint256 gdUBI) = reserve.mintInterestAndUBI(cDai, actualCDaiGains, actualCDaiGains);
+            (uint256 gdInterest, uint256 gdUBI) = reserve.mintInterestAndUBI(
+                cDai,
+                actualCDaiGains,
+                afterDonation
+            );
             // transfers the minted tokens to the given staking contract
             GoodDollar token = GoodDollar(address(avatar.nativeToken()));
             token.transfer(address(staking), gdInterest);
-            emit FundsTransferred(msg.sender,
-                                  address(staking),
-                                  address(reserve),
-                                  actualCDaiGains,
-                                  gdInterest,
-                                  gdUBI);
+            //transfer ubi to avatar on sidechain via bridge
+            token.transferAndCall(
+                tokenBridge,
+                gdUBI,
+                bytes32(uint256(homeAvatar))
+            );
+            emit FundsTransferred(
+                msg.sender,
+                address(staking),
+                address(reserve),
+                actualCDaiGains,
+                donated,
+                gdInterest,
+                gdUBI
+            );
         }
     }
 
     /**
-    * @dev making the contract inactive after it has transferred funds to `_avatar`
-    * only the avatar can destroy the contract.
-    * @param _avatar destination avatar address for funds
-    */
-    function end(Avatar _avatar)
-        public
-        onlyAvatar
-    {
+     * @dev making the contract inactive after it has transferred funds to `_avatar`
+     * only the avatar can destroy the contract.
+     * @param _avatar destination avatar address for funds
+     */
+    function end(Avatar _avatar) public onlyAvatar {
         uint256 remainingCDaiReserve = cDai.balanceOf(address(this));
         if (remainingCDaiReserve > 0) {
             cDai.transfer(address(_avatar), remainingCDaiReserve);

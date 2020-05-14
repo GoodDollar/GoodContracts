8cee4ce7ec5e4cf125a44823fdc114b706ab4e0b
Hadar Rottenberg <hadar@gooddollar.org>
2020-05-14 12:30:12 +0300

    add: contribution test and some refactoring

--- stakingModel/contracts/ContributionCalculation.sol
+++ stakingModel/contracts/ContributionCalculation.sol
@@ -5,30 +5,18 @@ import "../../contracts/dao/schemes/SchemeGuard.sol";
 import "./GoodMarketMaker.sol";
 import "./GoodReserveCDai.sol";
 import "../../contracts/DSMath.sol";
-import "../../contracts/token/GoodDollar.sol";
+
 
 /* @title Contribution calculation for selling gd tokens
  */
 contract ContributionCalculation is DSMath, SchemeGuard {
     using SafeMath for uint256;
 
-    GoodDollar public gooddollar;
     uint256 public sellContributionRatio;
 
-    event SellContributionRatioUpdated(address indexed caller,
-                                       uint256 nom,
-                                       uint256 denom);
+    event SellContributionRatioUpdated(address indexed caller, uint256 nom, uint256 denom);
 
-    constructor(
-        Avatar _avatar,
-        address _gooddollar,
-        uint256 _nom,
-        uint256 _denom
-    )
-        public
-        SchemeGuard(_avatar)
-    {
-        gooddollar = GoodDollar(_gooddollar);
+    constructor(Avatar _avatar, uint256 _nom, uint256 _denom) public SchemeGuard(_avatar) {
         sellContributionRatio = rdiv(_nom, _denom);
     }
 
@@ -38,32 +26,30 @@ contract ContributionCalculation is DSMath, SchemeGuard {
     @param _nom the nominator
     @param _denom the denominator
     */
-    function setContributionRatio(uint256 _nom, uint256 _denom)
-        external
-        onlyAvatar
-    {
+    function setContributionRatio(uint256 _nom, uint256 _denom) external onlyAvatar {
         sellContributionRatio = rdiv(_nom, _denom);
         emit SellContributionRatioUpdated(msg.sender, _nom, _denom);
     }
 
     /**
-    * @dev calculate the contribution amount during the sell action. there is a
-    * `sellContributionRatio` percent contribution
-    * @return (contributionAmount) the contribution amount for sell
-    */
-    function calculateContribution(GoodMarketMaker _marketMaker, GoodReserveCDai _reserve, address _contributer, ERC20 _token, uint256 _gdAmount)
-        external
-        view
-        returns (uint256)
-    {
-        uint256 decimalsDiff = uint256(27).sub(uint256(gooddollar.decimals()));
-        uint256 contribution =
-        rmul(
-                _gdAmount.mul(10**decimalsDiff), // expand to e27 precision
-                sellContributionRatio
-            )
-                .div(10**decimalsDiff); // return to e2 precision
+     * @dev calculate the amount after contribution during the sell action. there is a
+     * `sellContributionRatio` percent contribution
+     * @return (contributionAmount) the contribution amount for sell
+     */
+    function calculateContribution(
+        GoodMarketMaker _marketMaker,
+        GoodReserveCDai _reserve,
+        address _contributer,
+        ERC20 _token,
+        uint256 _gdAmount
+    ) external view returns (uint256) {
+        uint256 decimalsDiff = uint256(27).sub(2); //2 gooddollar decimals;
+        uint256 contribution = rmul(
+            _gdAmount.mul(10**decimalsDiff), // expand to e27 precision
+            sellContributionRatio
+        )
+            .div(10**decimalsDiff); // return to e2 precision
         require(_gdAmount > contribution, "Calculation error");
         return _gdAmount.sub(contribution);
     }
- }
+}

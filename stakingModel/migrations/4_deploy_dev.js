85b1f24f51c724285989f82e70ca3bcccef039ed
Or <or.dd100@gmail.com>
2020-06-04 17:38:51 +0300

    added test network

--- stakingModel/migrations/4_deploy_dev.js
+++ stakingModel/migrations/4_deploy_dev.js
@@ -6,11 +6,12 @@ const DAIMock = artifacts.require("./DAIMock.sol");
 const cDAIMock = artifacts.require("./cDAIMock.sol");
 const DaiFaucet = artifacts.require("./RopstenDaiFaucetMock.sol");
 const GoodDollar = artifacts.require("./GoodDollar.sol");
+const UBIScheme = artifacts.require("./UBIScheme.sol");
 
 module.exports = async function(deployer, network) {
   if (network.indexOf("production") >= 0) {
     return;
-  }  
+  }
   await deployer;
   const networkSettings = settings[network] || settings["default"];
   const accounts = await web3.eth.getAccounts();
@@ -18,23 +19,44 @@ module.exports = async function(deployer, network) {
   const dao_file = await fse.readFile("../releases/deployment.json", "utf8");
   const staking_deployment = await JSON.parse(staking_file);
   const dao_deployment = await JSON.parse(dao_file);
-  const staking_mainnet_addresses = staking_deployment[network + "-mainnet"];
-  const dao_sidechain_addresses = dao_deployment[network];
 
-  if (network.indexOf("mainnet") < 0) {
-    const goodDollar = await GoodDollar.at(dao_sidechain_addresses.GoodDollar);
+  let staking_mainnet_addresses, staking_sidechain_addresses, dao_sidechain_addresses;
 
-    await goodDollar.mint(accounts[0], "10000000");
+  if (network === "test") {
+    staking_mainnet_addresses = staking_deployment[network];
+    staking_sidechain_addresses = staking_deployment[network];
+    dao_sidechain_addresses = dao_deployment[network];
   }
   else {
-    const faucet = await DaiFaucet.at(networkSettings.daiFaucetAddress);
+    staking_mainnet_addresses = staking_deployment[network + "-mainnet"];
+    staking_sidechain_addresses = staking_deployment[network];
+    dao_sidechain_addresses = dao_deployment[network];
+  }
+
+  // not mainnet, including test
+  if (network.indexOf("mainnet") < 0) {
+    const goodDollar = await GoodDollar.at(dao_sidechain_addresses.GoodDollar);
+    const ubi = await UBIScheme.at(staking_sidechain_addresses.UBIScheme);
+
+    await goodDollar.mint(accounts[0], "10000000");
+    await goodDollar.transfer(ubi.address, "5000000");
+  }
+  
+  if (network.indexOf("mainnet") >= 0 || network === "test") {
     const dai = await DAIMock.at(staking_mainnet_addresses.DAI);
     const cDAI = await cDAIMock.at(staking_mainnet_addresses.cDAI);
     const simpleStaking = await StakingContract.at(staking_mainnet_addresses.DAIStaking);
     const goodReserve = await Reserve.at(staking_mainnet_addresses.Reserve);
 
-    console.log("get dai from faucet");
-    await faucet.allocateTo(accounts[0], web3.utils.toWei("100", "ether"));
+    if (network.indexOf("mainnet") >= 0) {
+      console.log("get dai from faucet");
+      const faucet = await DaiFaucet.at(networkSettings.daiFaucetAddress);
+      await faucet.allocateTo(accounts[0], web3.utils.toWei("100", "ether"));
+    }
+    else {
+      console.log("minting dai");
+      await dai.mint(accounts[0], web3.utils.toWei("100", "ether"));
+    }
 
     const approveStaking = dai.approve(simpleStaking.address, web3.utils.toWei("80", "ether"));
     const approveMinting = dai.approve(cDAI.address, web3.utils.toWei("20", "ether"));
@@ -66,6 +88,6 @@ module.exports = async function(deployer, network) {
     await Promise.all([preloadStaking, approveCdai]);
     
     console.log("preload reserve with CDAI");
-    await goodReserve.buy(cDAI.address, totalMinted.toString(), 0);
+    await goodReserve.buy(cDAI.address, (Math.floor(totalMinted.toNumber() / 2)).toString(), 0);
   }
 };

7143cfa230d3f602f387c4a7ed841ad01300a917
Or <or.dd100@gmail.com>
2020-06-05 04:06:04 +0300

    collecting interest

---
+++ stakingModel/migrations/5_collect_interest.js
@@ -0,0 +1,41 @@
+const fse = require("fs-extra");
+const settings = require("./deploy-settings.json");
+const StakingContract = artifacts.require("./SimpleDAIStaking.sol");
+const GoodFundsManager = artifacts.require("./GoodFundManager.sol");
+
+module.exports = async function(deployer, network) {
+  if (network.indexOf("production") >= 0) {
+    return;
+  }
+
+  await deployer;
+  const networkSettings = settings[network] || settings["default"];
+  const staking_file = await fse.readFile("releases/deployment.json", "utf8");
+  const staking_deployment = await JSON.parse(staking_file);
+
+  let staking_mainnet_addresses;
+
+  if (network === "develop") {
+    staking_mainnet_addresses = staking_deployment[network];
+  }
+  else if (network.indexOf("mainnet") >= 0) {
+    staking_mainnet_addresses = staking_deployment[network + "-mainnet"];
+  }
+
+  if (network.indexOf("mainnet") >= 0 || network === "develop") {
+    const simpleStaking = await StakingContract.at(staking_mainnet_addresses.DAIStaking);
+    const goodFundManager = await GoodFundsManager.at(staking_mainnet_addresses.FundManager);
+
+    const canCollect = await simpleStaking.canCollect();
+
+    if (network.indexOf("mainnet") >= 0 && !canCollect) return;
+
+    if (network === "develop" && !canCollect)
+        for (let i = 0; i < networkSettings.blockInterval; ++i)
+          await web3.currentProvider.send({ jsonrpc: "2.0", method: "evm_mine", id: 123 }, () => {});
+
+    console.log("collecting interest...");
+
+    await goodFundManager.transferInterest(simpleStaking.address);
+  }
+};

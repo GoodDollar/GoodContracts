5d37669a9a0913648a423178ecf5a49467a61c09
Or <or.dd100@gmail.com>
2020-06-11 20:33:06 +0300

    test fix

--- stakingModel/test/SimpleDAIStaking.test.js
+++ stakingModel/test/SimpleDAIStaking.test.js
@@ -517,10 +517,10 @@ contract("SimpleDAIStaking - staking with DAI mocks", ([founder, staker]) => {
   });
 
   it("should be able to be called once per withdrawInterval", async () => {
-    const canCollect = await simpleStaking.canCollect();
-    expect(canCollect).to.be.equal(false);
     // the block number difference between the calls are less then the block interval
     await simpleStaking.collectUBIInterest(founder).catch(e => e);
+    const canCollect = await simpleStaking.canCollect();
+    expect(canCollect).to.be.equal(false);
     const error = await simpleStaking.collectUBIInterest(founder).catch(e => e);
     expect(error.message).to.have.string("Need to wait for the next interval");
   });

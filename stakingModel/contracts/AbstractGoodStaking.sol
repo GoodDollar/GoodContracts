91d88c38f691de47d3e9db446375f68ed34bdd7e
--global <--global>
2020-06-25 13:23:36 +0530

    Updated documentation in AbstractGoodStaking.sol

--- stakingModel/contracts/AbstractGoodStaking.sol
+++ stakingModel/contracts/AbstractGoodStaking.sol
@@ -8,15 +8,35 @@ pragma solidity 0.5.4;
  */
 contract AbstractGoodStaking {
 
+    /**
+     * @dev Structure to store staking details.
+     * It contains amount of tokens staked and blocknumber at which last staked.
+     */
     struct Staker {
         uint256 stakedToken;
         uint256 lastStake;
     }
 
+    /**
+     * @dev Mapping to store staking details for each user.
+     */
     mapping(address => Staker) public stakers;
 
+    /**
+     * @dev Emitted when `staker` stake `value` tokens
+     */
     event Staked(address indexed staker, uint256 value);
+
+    /**
+     * @dev Emitted when `staker` withdraws their stake `value` tokens from 
+     * `actual` contract balance.
+     */
     event StakeWithdraw(address indexed staker, uint256 value, uint256 actual);
+
+    /**
+     * @dev Emitted when fundmanager transfers intrest collected from defi protrocol.
+     * `recipient` will receive `intrestTokenValue` as intrest.
+     */
     event InterestCollected(
         address recipient,
         uint256 intrestTokenValue,
@@ -39,10 +59,20 @@ contract AbstractGoodStaking {
         
     }
 
+    /**
+     * @dev calculates the holding of intrestToken by staking contract in terms of token value.
+     * @returns It will return the token worth of intrest token that contract is holding.
+     */
     function currentDAIWorth() external view returns (uint256) {
 
     }
 
+    /**
+     * @dev calculates the tokenGain, intrestTokenGain and precisionLossToken
+     * @returns Intrest gained on lending the tokens.
+     * @returns Intrest gained on lending the tokens in terms of token rate.
+     * @returns Token's precision loss due to decimal difference.
+     */
     function currentUBIInterest()
         external
         view
@@ -58,6 +88,10 @@ contract AbstractGoodStaking {
     /**
      * @dev collect gained interest by fundmanager
      * @param recipient of intrestToken gains
+     * @returns Intrest gained on lending the tokens.
+     * @returns Intrest gained on lending the tokens in terms of token rate.
+     * @returns Token's precision loss due to decimal difference.
+     * @returns average intrest donation ratio.
      */
     function collectUBIInterest(address recipient)
         external

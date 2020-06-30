9670cf2293494f0a9da0677fcc97817b1a2c6897
PremSOMISH <prem@somish.com>
2020-06-30 16:43:28 +0530

    Updated based on suggestions on PR

--- stakingModel/contracts/AbstractGoodStaking.sol
+++ stakingModel/contracts/AbstractGoodStaking.sol
@@ -8,17 +8,38 @@ pragma solidity 0.5.4;
  */
 contract AbstractGoodStaking {
 
+    /**
+     * @dev Structure to store staking details.
+     * It contains amount of tokens staked and blocknumber at which last staked.
+     */
     struct Staker {
         uint256 stakedToken;
         uint256 lastStake;
     }
 
+    /**
+     * @dev Mapping to store staking details for each user.
+     */
     mapping(address => Staker) public stakers;
 
-    event Staked(address indexed staker, uint256 value);
-    event StakeWithdraw(address indexed staker, uint256 value, uint256 actual);
+    /**
+     * @dev Emitted when `staker` stake `value` tokens of `token`
+     */
+    event Staked(address indexed staker, address token, uint256 value);
+
+    /**
+     * @dev Emitted when `staker` withdraws their stake `value` tokens from 
+     * `startingBalance` of contract.
+     */
+    event StakeWithdraw(address indexed staker, address token, uint256 value, uint256 startingBalance);
+
+    /**
+     * @dev Emitted when fundmanager transfers intrest collected from defi protrocol.
+     * `recipient` will receive `intrestTokenValue` as intrest.
+     */
     event InterestCollected(
         address recipient,
+        address token,
         uint256 intrestTokenValue,
         uint256 tokenValue,
         uint256 tokenPrecisionLoss
@@ -39,10 +60,20 @@ contract AbstractGoodStaking {
         
     }
 
-    function currentDAIWorth() external view returns (uint256) {
+    /**
+     * @dev calculates the holding of intrestToken by staking contract in terms of token value.
+     * @return It will return the token worth of intrest token that contract is holding.
+     */
+    function currentTokenWorth() external view returns (uint256) {
 
     }
 
+    /**
+     * @dev calculates the tokenGain, intrestTokenGain and precisionLossToken
+     * @return Intrest gained on lending the tokens.
+     * @return Intrest gained on lending the tokens in terms of token rate.
+     * @return Token's precision loss due to decimal difference.
+     */
     function currentUBIInterest()
         external
         view
@@ -58,6 +89,10 @@ contract AbstractGoodStaking {
     /**
      * @dev collect gained interest by fundmanager
      * @param recipient of intrestToken gains
+     * @return Intrest gained on lending the tokens.
+     * @return Intrest gained on lending the tokens in terms of token rate.
+     * @return Token's precision loss due to decimal difference.
+     * @return average intrest donation ratio.
      */
     function collectUBIInterest(address recipient)
         external
@@ -70,4 +105,22 @@ contract AbstractGoodStaking {
     {
         
     }
+
+    /**
+     * @dev Invests staked tokens to defi protocol.
+     * @param amount tokens staked.
+     */
+    function mint(uint amount) internal {}
+
+    /**
+     * @dev Redeem invested tokens from defi protocol.
+     * @param amount tokens to be redeemed.
+     */
+    function redeem(uint amount) internal {}
+
+    /**
+     * @dev Calculates exchange rate for token to intrest token from defi protocol.
+     * @return exchange rate.
+     */
+    function exchangeRate() internal view returns(uint) {}
 }

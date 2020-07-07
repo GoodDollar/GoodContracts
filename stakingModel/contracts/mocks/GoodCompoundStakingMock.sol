40b0c7b8058affb7843b998b08b9075b48ed5ad0
PremSOMISH <prem@somish.com>
2020-07-07 18:19:41 +0530

    Changes in mock files

---
+++ stakingModel/contracts/mocks/GoodCompoundStakingMock.sol
@@ -0,0 +1,37 @@
+pragma solidity 0.5.4;
+
+import "../GoodCompoundStaking.sol";
+
+
+/**
+ * @title A GoodCompoundStaking mock.
+ * return different donation ratio on collect.
+ */
+contract GoodCompoundStakingMock is GoodCompoundStaking {
+    constructor(
+        address _token,
+        address _iToken,
+        address _fundManager,
+        uint256 _blockInterval,
+        Avatar _avatar,
+        Identity _identity
+    )
+        public
+        GoodCompoundStaking(_token, _iToken, _fundManager, _blockInterval, _avatar, _identity)
+    {}
+
+    function collectUBIInterest(address recipient)
+        public
+        onlyFundManager
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint32
+        )
+    {
+        (uint256 iTokenGains, uint256 tokenGains, uint256 precisionLossToken, ) = super
+            .collectUBIInterest(recipient);
+        return (iTokenGains, tokenGains, precisionLossToken, 8e5);
+    }
+}

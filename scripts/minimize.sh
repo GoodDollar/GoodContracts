d0bdcde3a1883a1963b1ecd08b78633e68c6fce5
sirpy <hadarr@gmail.com>
2021-01-27 20:20:33 +0200

    chore: adapt minimize to hardhat

--- scripts/minimize.sh
+++ scripts/minimize.sh
@@ -3,15 +3,14 @@
 # Notes:
 #  - Shchema: https://github.com/trufflesuite/truffle/tree/develop/packages/truffle-contract-schema
 #  - bytecode vs deployedBytecode: https://ethereum.stackexchange.com/questions/32234/difference-between-bytecode-and-runtime-bytecode
-for i in "$1"/*.json;do
+# FILES=`find $1 -name '*.json' ! -name '*.dbg.json'`
+find $1 -name "*.json" ! -name '*.*.json' |while read fname; do
+  echo "$fname"
   #[ -z "${i%%*.min.json}" ] && continue # already minified
-  if [[ $i == *.min.json ]] 
-  then
-    continue
-  fi
-  m=${i%%.json}.min.json
-  echo "Minimizing truffle json artifact: $i"
-  echo "Original size: $(wc -c "$i")"
-  ./node_modules/node-jq/bin/jq 'del(.ast,.legacyAST,.source,.deployedSourceMap,.userdoc,.sourcePath)' $i > $m
+  f=${fname##*/}
+  m=./build/contracts/${f%%.json}.min.json
+  echo "Minimizing contract json artifact: $fname"
+  echo "Original size: $(wc -c "$fname")"
+  ./node_modules/node-jq/bin/jq -c 'del(.bytecode,.deployedBytecode,.ast,.legacyAST,.source,.deployedSourceMap,.userdoc,.sourcePath)' $fname > $m
   echo "Minimized size: $(wc -c "$m")"
 done

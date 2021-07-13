b4c7aa19dfcec1452e6456767f403610bcde1b7f
sirpy <hadarr@gmail.com>
2021-07-13 15:44:19 +0300

    fix: fusestaking via fusd

--- upgradables/contracts/staking/FuseStakingV3.sol
+++ upgradables/contracts/staking/FuseStakingV3.sol
@@ -9,7 +9,8 @@ import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
 import "../utils/DSMath.sol";
 
 import "../Interfaces.sol";
-import "hardhat/console.sol";
+
+// import "hardhat/console.sol";
 
 interface IConsensus {
 	/**
@@ -36,6 +37,20 @@ interface IConsensus {
 		external
 		view
 		returns (address[] memory);
+}
+
+interface PegSwap {
+	/**
+	 * @notice exchanges the source token for target token
+	 * @param sourceAmount count of tokens being swapped
+	 * @param source the token that is being given
+	 * @param target the token that is being taken
+	 */
+	function swap(
+		uint256 sourceAmount,
+		address source,
+		address target
+	) external;
 }
 
 contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
@@ -66,6 +81,11 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 	address public USDC;
 	address public fUSD;
 
+	bool public paused;
+	address public guardian;
+
+	PegSwap pegSwap;
+
 	event UBICollected(
 		uint256 indexed currentDay,
 		uint256 ubi, //G$ sent to ubischeme
@@ -86,6 +106,16 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 			address(0x3014ca10b91cb3D0AD85fEf7A3Cb95BCAc9c0f79)
 		);
 		validators.push(address(0xcb876A393F05a6677a8a029f1C6D7603B416C0A6));
+	}
+
+	modifier notPaused() {
+		require(paused == false, "ubi collection is pauased");
+		_;
+	}
+
+	modifier onlyGuardian() {
+		require(msg.sender == guardian, "not guardian");
+		_;
 	}
 
 	function upgrade0() public {
@@ -125,6 +155,25 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 			USDC = address(0x620fd5fa44BE6af63715Ef4E65DDFA0387aD13F5);
 			fUSD = address(0x249BE57637D8B013Ad64785404b24aeBaE9B098B);
 		}
+	}
+
+	function upgrade3() public {
+		if (guardian == address(0)) {
+			paused = true;
+			guardian = address(0x5128E3C1f8846724cc1007Af9b4189713922E4BB);
+		}
+	}
+
+	function upgrade4() public {
+		if (address(pegSwap) == address(0)) {
+			pegSwap = PegSwap(0xdfE016328E7BcD6FA06614fE3AF3877E931F7e0a);
+			paused = false;
+		}
+	}
+
+	function upgrade5() public {
+		cERC20(fUSD).approve(address(pegSwap), type(uint256).max);
+		cERC20(USDC).approve(address(uniswap), type(uint256).max);
 	}
 
 	function setContracts(address _gd, address _ubischeme) public onlyOwner {
@@ -178,8 +227,10 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 		);
 		uint256 perValidator = _value.div(validators.length);
 		for (uint256 i = 0; i < validators.length; i++) {
-			uint256 cur =
-				consensus.delegatedAmount(address(this), validators[i]);
+			uint256 cur = consensus.delegatedAmount(
+				address(this),
+				validators[i]
+			);
 			if (cur == 0) continue;
 			if (cur <= perValidator) {
 				undelegateWithCatch(validators[i], cur);
@@ -213,12 +264,15 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 			return true;
 		}
 
-		uint256 perValidator =
-			totalDelegated().add(_value).div(validators.length);
+		uint256 perValidator = totalDelegated().add(_value).div(
+			validators.length
+		);
 		uint256 left = _value;
 		for (uint256 i = 0; i < validators.length && left > 0; i++) {
-			uint256 cur =
-				consensus.delegatedAmount(address(this), validators[i]);
+			uint256 cur = consensus.delegatedAmount(
+				address(this),
+				validators[i]
+			);
 
 			if (cur < perValidator) {
 				uint256 toDelegate = perValidator.sub(cur);
@@ -238,16 +292,20 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 	function totalDelegated() public view returns (uint256) {
 		uint256 total = 0;
 		for (uint256 i = 0; i < validators.length; i++) {
-			uint256 cur =
-				consensus.delegatedAmount(address(this), validators[i]);
+			uint256 cur = consensus.delegatedAmount(
+				address(this),
+				validators[i]
+			);
 			total = total.add(cur);
 		}
 		return total;
 	}
 
 	function removeValidator(address _validator) public onlyOwner {
-		uint256 delegated =
-			consensus.delegatedAmount(address(this), _validator);
+		uint256 delegated = consensus.delegatedAmount(
+			address(this),
+			_validator
+		);
 		if (delegated > 0) {
 			uint256 prevBalance = balance();
 			undelegateWithCatch(_validator, delegated);
@@ -269,7 +327,7 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 		}
 	}
 
-	function collectUBIInterest() public {
+	function collectUBIInterest() public notPaused {
 		uint256 curDay = ubischeme.currentDay();
 		require(
 			curDay != lastDayCollected,
@@ -280,12 +338,14 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 		require(pendingFuseEarnings + earnings > 0, "no earnings to collect");
 
 		lastDayCollected = curDay;
-		uint256 fuseUBI =
-			earnings.mul(RATIO_BASE - stakeBackRatio).div(RATIO_BASE);
+		uint256 fuseUBI = earnings.mul(RATIO_BASE - stakeBackRatio).div(
+			RATIO_BASE
+		);
 		uint256 stakeBack = earnings - fuseUBI;
 
-		uint256[] memory fuseswapResult =
-			_buyGD(fuseUBI.add(pendingFuseEarnings)); //buy GD with X% of earnings
+		uint256[] memory fuseswapResult = _buyGD(
+			fuseUBI.add(pendingFuseEarnings)
+		); //buy GD with X% of earnings
 		pendingFuseEarnings = fuseUBI.add(pendingFuseEarnings).sub(
 			fuseswapResult[0]
 		);
@@ -295,11 +355,10 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 		uint256 keeperFee = gdBought.mul(keeperFeeRatio).div(RATIO_BASE);
 		if (keeperFee > 0) GD.transfer(msg.sender, keeperFee);
 
-		uint256 communityPoolContribution =
-			gdBought
-				.sub(keeperFee) //subtract fee
-				.mul(communityPoolRatio) // * ommunityPoolRatio
-				.div(RATIO_BASE); // = G$ after fee * communityPoolRatio%
+		uint256 communityPoolContribution = gdBought
+		.sub(keeperFee) //subtract fee
+		.mul(communityPoolRatio) // * ommunityPoolRatio
+		.div(RATIO_BASE); // = G$ after fee * communityPoolRatio%
 
 		uint256 ubiAfterFeeAndPool = gdBought.sub(communityPoolContribution);
 
@@ -328,34 +387,83 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 	function _buyGD(uint256 _value) internal returns (uint256[] memory) {
 		//buy from uniwasp
 		require(_value > 0, "buy value should be > 0");
-		uint256 maxFuse = calcMaxFuseWithPriceImpact(_value);
-		uint256 maxFuseUSDC = calcMaxFuseUSDCWithPriceImpact(_value);
+		(uint256 maxFuse, uint256 fuseGDOut) = calcMaxFuseWithPriceImpact(
+			_value
+		);
+		(
+			uint256 maxFuseUSDC,
+			uint256 usdcGDOut
+		) = calcMaxFuseUSDCWithPriceImpact(_value);
 		address[] memory path;
 		if (maxFuse >= maxFuseUSDC) {
 			path = new address[](2);
 			path[1] = address(GD);
 			path[0] = uniswap.WETH();
+			return
+				uniswap.swapExactETHForTokens{ value: maxFuse }(
+					(fuseGDOut * 95) / 100,
+					path,
+					address(this),
+					now
+				);
 		} else {
-			maxFuse = maxFuseUSDC;
-			path = new address[](4);
-			path[3] = address(GD);
-			path[2] = USDC;
-			path[1] = fUSD;
-			path[0] = uniswap.WETH();
-		}
-		return
-			uniswap.swapExactETHForTokens{ value: maxFuse }(
-				0,
+			(uint256 usdcAmount, uint256 usedFuse) = _buyUSDC(maxFuseUSDC);
+			path = new address[](2);
+			path[1] = address(GD);
+			path[0] = USDC;
+
+			uint256[] memory result = uniswap.swapExactTokensForTokens(
+				usdcAmount,
+				(usdcGDOut * 95) / 100,
 				path,
 				address(this),
 				now
 			);
+			//buyGD should return how much fuse was used in [0] and how much G$ we got in [1]
+			result[0] = usedFuse;
+			return result;
+		}
+	}
+
+	/**
+	 * @dev internal method to buy USDC via fuse->fusd
+	 * @param _fuseIn fuse to be sold
+	 * @return usdcAmount and usedFuse how much usdc we got and how much fuse was used
+	 */
+
+	function _buyUSDC(uint256 _fuseIn)
+		internal
+		returns (uint256 usdcAmount, uint256 usedFuse)
+	{
+		//buy from uniwasp
+		require(_fuseIn > 0, "buy value should be > 0");
+		UniswapPair uniswapFUSEfUSDPair = UniswapPair(
+			uniswapFactory.getPair(uniswap.WETH(), fUSD)
+		); //fusd is pegged 1:1 to usdc
+		(uint256 r_fuse, uint256 r_fusd, ) = uniswapFUSEfUSDPair.getReserves();
+
+		(uint256 maxFuse, uint256 tokenOut) = calcMaxTokenWithPriceImpact(
+			r_fuse,
+			r_fusd,
+			_fuseIn
+		); //expect r_token to be in 18 decimals
+
+		address[] memory path = new address[](2);
+		path[1] = fUSD;
+		path[0] = uniswap.WETH();
+		uint256[] memory result = uniswap.swapExactETHForTokens{
+			value: maxFuse
+		}((tokenOut * 95) / 100, path, address(this), now);
+
+		pegSwap.swap(result[1], fUSD, USDC);
+		usedFuse = result[0];
+		usdcAmount = result[1] / 1e12; //convert fusd from 1e18 to usdc 1e6
 	}
 
 	function calcMaxFuseWithPriceImpact(uint256 _value)
 		public
 		view
-		returns (uint256)
+		returns (uint256 fuseAmount, uint256 tokenOut)
 	{
 		(uint256 r_fuse, uint256 r_gd, ) = uniswapPair.getReserves();
 
@@ -365,12 +473,14 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 	function calcMaxFuseUSDCWithPriceImpact(uint256 _value)
 		public
 		view
-		returns (uint256 maxFuse)
+		returns (uint256 maxFuse, uint256 gdOut)
 	{
-		UniswapPair uniswapFUSEfUSDPair =
-			UniswapPair(uniswapFactory.getPair(uniswap.WETH(), fUSD)); //fusd is pegged 1:1 to usdc
-		UniswapPair uniswapGDUSDCPair =
-			UniswapPair(uniswapFactory.getPair(address(GD), USDC));
+		UniswapPair uniswapFUSEfUSDPair = UniswapPair(
+			uniswapFactory.getPair(uniswap.WETH(), fUSD)
+		); //fusd is pegged 1:1 to usdc
+		UniswapPair uniswapGDUSDCPair = UniswapPair(
+			uniswapFactory.getPair(address(GD), USDC)
+		);
 		(uint256 rg_gd, uint256 rg_usdc, ) = uniswapGDUSDCPair.getReserves();
 		(uint256 r_fuse, uint256 r_fusd, ) = uniswapFUSEfUSDPair.getReserves();
 		uint256 fusdPriceInFuse = r_fuse.mul(1e18).div(r_fusd); //fusd is 1e18 so to keep in original 1e18 precision we first multiply by 1e18
@@ -386,10 +496,13 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 		uint256 fuseValueInfUSD = _value.mul(1e18).div(fusdPriceInFuse); //value and usdPriceInFuse are in 1e18, we mul by 1e18 to keep 18 decimals precision
 		// console.log("fuse fusd value: %s", fuseValueInfUSD);
 
-		uint256 maxUSDC =
-			calcMaxTokenWithPriceImpact(rg_usdc * 1e12, rg_gd, fuseValueInfUSD); //expect r_token to be in 18 decimals
+		(uint256 maxUSDC, uint256 tokenOut) = calcMaxTokenWithPriceImpact(
+			rg_usdc * 1e12,
+			rg_gd,
+			fuseValueInfUSD
+		); //expect r_token to be in 18 decimals
 		// console.log("max USDC: %s", maxUSDC);
-
+		gdOut = tokenOut;
 		maxFuse = maxUSDC.mul(fusdPriceInFuse).div(1e18); //both are in 1e18 precision, div by 1e18 to keep precision
 	}
 
@@ -414,24 +527,24 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 		uint256 r_token,
 		uint256 r_gd,
 		uint256 _value
-	) public view returns (uint256) {
+	) public view returns (uint256 fuseAmount, uint256 tokenOut) {
 		uint256 start = 0;
 		uint256 end = _value.div(1e18); //save iterations by moving precision to whole Fuse quantity
 		// uint256 curPriceWei = uint256(1e18).mul(r_gd) / r_token; //uniswap quote  formula UniswapV2Library.sol
 		uint256 gdForQuantity = getAmountOut(1e18, r_token, r_gd);
-		uint256 priceForQuantityWei =
-			rdiv(1e18, gdForQuantity.mul(1e16)).div(1e9);
-		uint256 maxPriceWei =
-			priceForQuantityWei.mul(RATIO_BASE.add(maxSlippageRatio)).div(
-				RATIO_BASE
-			);
+		uint256 priceForQuantityWei = rdiv(1e18, gdForQuantity.mul(1e16)).div(
+			1e9
+		);
+		uint256 maxPriceWei = priceForQuantityWei
+		.mul(RATIO_BASE.add(maxSlippageRatio))
+		.div(RATIO_BASE);
 		// console.log(
 		// 	"curPrice: %s, maxPrice %s",
 		// 	priceForQuantityWei,
 		// 	maxPriceWei
 		// );
-		uint256 fuseAmount = _value;
-
+		fuseAmount = _value;
+		tokenOut;
 		//Iterate while start not meets end
 		while (start <= end) {
 			// Find the mid index
@@ -439,7 +552,7 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 			if (midQuantityWei == 0) break;
 			gdForQuantity = getAmountOut(midQuantityWei, r_token, r_gd);
 			priceForQuantityWei = rdiv(midQuantityWei, gdForQuantity.mul(1e16))
-				.div(1e9);
+			.div(1e9);
 			// console.log(
 			// 	"gdForQuantity: %s, priceForQuantity: %s, midQuantity: %s",
 			// 	gdForQuantity,
@@ -449,10 +562,9 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 			if (priceForQuantityWei <= maxPriceWei) {
 				start = midQuantityWei.div(1e18) + 1; //reduce precision to whole quantity div 1e18
 				fuseAmount = midQuantityWei;
+				tokenOut = gdForQuantity;
 			} else end = midQuantityWei.div(1e18) - 1; //reduce precision to whole quantity div 1e18
 		}
-
-		return fuseAmount;
 	}
 
 	function undelegateWithCatch(address _validator, uint256 _amount)
@@ -480,6 +592,10 @@ contract FuseStakingV3 is Initializable, OwnableUpgradeable, DSMath {
 
 	function balance() internal view returns (uint256) {
 		return payable(address(this)).balance;
+	}
+
+	function setPaused(bool _paused) public onlyGuardian {
+		paused = _paused;
 	}
 
 	receive() external payable {}
